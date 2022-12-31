debd1f2f164b6647a906ac0f7c607f453d5d593c
Lynx Binaryz <121704568+lynxbinz@users.noreply.github.com>
2022-12-31 13:39:01 +0700

    Create pyScript.py

---
+++ pyScript.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+def telegram_bot_msgSender(msg):
+    teleBot_token = 'xxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    teleBot_chatID = 'xxxxxxxxxxxx'
+    send_msg = 'https://api.telegram.org/bot' + teleBot_token + '/sendMessage?chat_id=' + teleBot_chatID + '&parse_mode=Markdown&text=' + msg
+
+    response = requests.get(send_msg)
+
+    return response.json()
+
+telegram_bot_msgSender(sys.argv[1])
