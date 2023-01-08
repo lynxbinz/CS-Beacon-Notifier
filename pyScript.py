aa3f4ddf1ca565c9f297431178bdf5c2500e8b34
Lynx Binaryz <121704568+lynxbinz@users.noreply.github.com>
2023-01-08 18:59:42 +0700

    Update pyScript.py
    Beautified the code.

--- pyScript.py
+++ pyScript.py
@@ -4,12 +4,17 @@ import requests
 import sys
 
 def telegram_bot_msgSender(msg):
-    teleBot_token = 'xxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    teleBot_chatID = 'xxxxxxxxxxxx'
-    send_msg = 'https://api.telegram.org/bot' + teleBot_token + '/sendMessage?chat_id=' + teleBot_chatID + '&parse_mode=Markdown&text=' + msg
-
+    teleBot_token = "xxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    teleBot_chatID = "xxxxxxxxxxxx"
+    send_msg = (
+        "https://api.telegram.org/bot"
+        + teleBot_token
+        + "/sendMessage?chat_id="
+        + teleBot_chatID
+        + "&parse_mode=Markdown&text="
+        + msg
+    )
     response = requests.get(send_msg)
-
     return response.json()
 
 telegram_bot_msgSender(sys.argv[1])
